e700e3c5d24560b262f450810debfe843b2b8fc0
Jose Vila <you@example.comJDVila@gmail.com>
2017-03-07 17:24:13 -0500

    Refactor ActivityLogin to follow Model-View-Presenter

---
+++ app/src/main/java/com/rooksoto/parallel/view/activityLogin/FragmentLoginLoginContract.java
@@ -0,0 +1,18 @@
+package com.rooksoto.parallel.view.activityLogin;
+
+import com.rooksoto.parallel.BasePresenter;
+import com.rooksoto.parallel.BaseView;
+
+interface FragmentLoginLoginContract {
+
+    interface View extends BaseView<Presenter> {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+        //FragmentLoginLogin
+
+    }
+
+}

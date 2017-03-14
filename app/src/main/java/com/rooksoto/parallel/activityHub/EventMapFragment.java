f3a5db5b32805a57c2a2cff0b08313e08560ea79
nuily <mslinhui@gmail.com>
2017-03-14 18:36:13 -0400

    load large image to pan

--- app/src/main/java/com/rooksoto/parallel/activityHub/EventMapFragment.java
+++ app/src/main/java/com/rooksoto/parallel/activityHub/EventMapFragment.java
@@ -4,59 +4,52 @@ package com.rooksoto.parallel.activityHub;
  * Created by rook on 3/2/17.
  */
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.rooksoto.parallel.R;
-import com.rooksoto.parallel.utility.geolocation.ParallelLocation;
 
-// THIS FRAGMENT IS NOW USELESS
-
-public class FragmentHubLocation extends Fragment {
-
-    private View mView;
-    private TextView tvLatitude;
-    private TextView tvLongitude;
-
-    private ParallelLocation locationService = null;
+public class EventMapFragment extends Fragment {
+    private View rootView;
+    private SubsamplingScaleImageView imageView;
 
     @Override
     public void onCreate (@Nullable Bundle savedInstanceState) {
-        locationService = ParallelLocation.getInstance();
         super.onCreate(savedInstanceState);
     }
 
     @Nullable
     @Override
     public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mView = inflater.inflate(R.layout.fragment_hub_location, container, false);
-
-        tvLatitude = (TextView) mView.findViewById(R.id.tv_Latitude);
-        tvLongitude = (TextView) mView.findViewById(R.id.tv_Longitude);
-
-        return mView;
+        rootView = inflater.inflate(R.layout.fragment_event_map, container, false);
+//        PinView imageView = (PinView)rootView.findViewById(id.imageView);
+//        imageView.setImage(ImageSource.asset("squirrel.jpg"));
+//        imageView.setPin(new PointF(1718f, 581f));
+        return rootView;
     }
 
     @Override
     public void onViewCreated (View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        imageView = (SubsamplingScaleImageView)rootView.findViewById(R.id.imageView);
+        imageView.setImage(ImageSource.resource(R.drawable.floorplan));
+//        imageView.setImage(ImageSource.asset("map.png"))
+//        imageView.setImage(ImageSource.uri("/sdcard/DCIM/DSCM00123.JPG"));
     }
 
     @Override
     public void onResume () {
         super.onResume();
-        tvLatitude.setText(String.valueOf(locationService.getLatitude()));
-        tvLongitude.setText(String.valueOf(locationService.getLongitude()));
     }
 
     @Override
     public void onStop () {
         super.onStop();
-        locationService.disconnect();
     }
 }

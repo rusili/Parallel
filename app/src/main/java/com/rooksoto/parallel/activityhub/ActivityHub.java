500ee19448a24ff51a486963859cc6f77f8db2bd
rusili <rusili56@gmail.com>
2017-03-16 22:44:34 -0400

    to merge

--- app/src/main/java/com/rooksoto/parallel/activityhub/ActivityHub.java
+++ app/src/main/java/com/rooksoto/parallel/activityhub/ActivityHub.java
@@ -1,7 +1,6 @@
-package com.rooksoto.parallel.activityhub;
+package com.rooksoto.parallel.activityHub;
 
 import android.content.res.Resources;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -10,22 +9,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
-import com.eftimoff.viewpagertransformers.DepthPageTransformer;
+import com.eftimoff.viewpagertransformers.TabletTransformer;
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.ogaclejapan.smarttablayout.utils.v13.FragmentPagerItem;
 import com.ogaclejapan.smarttablayout.utils.v13.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v13.FragmentPagerItems;
-import com.readystatesoftware.systembartint.SystemBarTintManager;
 import com.rooksoto.parallel.R;
-import com.rooksoto.parallel.activityhub.attendees.FragmentAttendees;
-import com.rooksoto.parallel.activityhub.chat.FragmentChat;
-import com.rooksoto.parallel.activityhub.enterid.FragmentHubEnterID;
-import com.rooksoto.parallel.activityhub.eventmap.FragmentEventMap;
-import com.rooksoto.parallel.activityhub.itinerary.FragmentItinerary;
-import com.rooksoto.parallel.activityhub.profile.FragmentProfile;
-import com.rooksoto.parallel.utility.CustomAlertDialog;
+import com.rooksoto.parallel.activityHub.attendees.FragmentAttendees;
+import com.rooksoto.parallel.activityHub.chat.FragmentChat;
+import com.rooksoto.parallel.activityHub.eventinfo.FragmentEventInfo;
 import com.rooksoto.parallel.utility.geolocation.ParallelLocation;
 import com.rooksoto.parallel.utility.widgets.camera2.Camera2BasicFragment;
 
@@ -34,7 +27,7 @@ public class ActivityHub extends AppCompatActivity implements ActivityHubPresent
 
     private View view;
 
-    private int containerID = R.id.content_frame;
+    private int containerID = R.id.viewpager;
     private static final String TAG = "ActivityHub";
     private ViewPager viewPager;
     private SmartTabLayout viewPagerTab;
@@ -48,17 +41,15 @@ public class ActivityHub extends AppCompatActivity implements ActivityHubPresent
         initialize();
     }
 
-    public void setupViewpager() {
+    private void setupViewpager() {
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
-        viewPagerTab.setVisibility(View.VISIBLE);
 
         pages = new FragmentPagerItems(this);
-        pages.add(FragmentPagerItem.of("Itinerary", FragmentItinerary.class));
-        pages.add(FragmentPagerItem.of("Profile", FragmentProfile.class));
-        pages.add(FragmentPagerItem.of("Attendees", FragmentAttendees.class));
+        pages.add(FragmentPagerItem.of("Event Map", EventMapFragment.class));
+        pages.add(FragmentPagerItem.of("Event Info", FragmentEventInfo.class));
         pages.add(FragmentPagerItem.of("Chat", FragmentChat.class));
-        pages.add(FragmentPagerItem.of("Map", FragmentEventMap.class));
+        pages.add(FragmentPagerItem.of("Attendees", FragmentAttendees.class));
         pages.add(FragmentPagerItem.of("Camera", Camera2BasicFragment.class));
 
         FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(getFragmentManager(), pages);
@@ -67,39 +58,36 @@ public class ActivityHub extends AppCompatActivity implements ActivityHubPresent
         final Resources res = view.getResources();
 
         viewPagerTab.setCustomTabView(new SmartTabLayout.TabProvider() {
-              @Override
-              public View createTabView(ViewGroup container, int position, PagerAdapter adapter) {
-                  ImageView icon = (ImageView) inflater.inflate(R.layout.custom_tab_icon, container,
-                          false);
-                  switch (position) {
-                      case 0:
-                          icon.setImageDrawable(res.getDrawable(R.drawable.ic_today));
-                          break;
-                      case 1:
-                          icon.setImageDrawable(res.getDrawable(R.drawable.ic_person_outline_black_24dp));
-                          break;
-                      case 2:
-                          icon.setImageDrawable(res.getDrawable(R.drawable.ic_people_outline));
-                          break;
-                      case 3:
-                          icon.setImageDrawable(res.getDrawable(R.drawable.ic_chat_bubble_outline));
-                          break;
-                      case 4:
-                          icon.setImageDrawable(res.getDrawable(R.drawable.ic_map_black));
-                          break;
-                      case 5:
-                          icon.setImageDrawable(res.getDrawable(R.drawable.ic_photo_camera_black_24dp));
-                          break;
-                      default:
-                          throw new IllegalStateException("Invalid position: " + position);
-                  }
-                  return icon;
-              }
-            }
+                                          @Override
+                                          public View createTabView(ViewGroup container, int position, PagerAdapter adapter) {
+                                              ImageView icon = (ImageView) inflater.inflate(R.layout.custom_tab_icon, container,
+                                                      false);
+                                              switch (position) {
+                                                  case 0:
+                                                      icon.setImageDrawable(res.getDrawable(R.drawable.ic_map_black));
+                                                      break;
+                                                  case 1:
+                                                      icon.setImageDrawable(res.getDrawable(R.drawable.ic_today));
+                                                      break;
+                                                  case 2:
+                                                      icon.setImageDrawable(res.getDrawable(R.drawable.ic_chat_bubble_outline));
+                                                      break;
+                                                  case 3:
+                                                      icon.setImageDrawable(res.getDrawable(R.drawable.ic_people_outline));
+                                                      break;
+                                                  case 4:
+                                                      icon.setImageDrawable(res.getDrawable(R.drawable.ic_panorama_wide_angle));
+                                                      break;
+                                                  default:
+                                                      throw new IllegalStateException("Invalid position: " + position);
+                                              }
+                                              return icon;
+                                          }
+                                      }
         );
 
         viewPager.setAdapter(adapter);
-        viewPager.setPageTransformer(true, new DepthPageTransformer());
+        viewPager.setPageTransformer(true, new TabletTransformer());
         viewPagerTab.setViewPager(viewPager);
     }
 
@@ -111,18 +99,7 @@ public class ActivityHub extends AppCompatActivity implements ActivityHubPresent
     public void initialize() {
         view = getWindow().getDecorView().getRootView();
         activityHubPresenter.onInitialize();
-        //loadFragmentEnterID();
-        activityHubPresenter.toViewPager();
-    }
-
-    private void loadFragmentEnterID () {
-        SmartTabLayout smartTabLayout = (SmartTabLayout) findViewById(R.id.viewpagertab);
-        smartTabLayout.setVisibility(View.INVISIBLE);
-
-        getFragmentManager().beginTransaction()
-                .setCustomAnimations(R.animator.animator_fade_in, R.animator.animator_fade_out)
-                .add(containerID, new FragmentHubEnterID(this), "Enter ID")
-                .commit();
+        setupViewpager();
     }
 
     @Override
